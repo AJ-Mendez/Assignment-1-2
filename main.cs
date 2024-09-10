d615bc3be4c216b96d4f6c76f9cd2addc7e33e6f
AJ-Mendez <armred03@gmail.com>
2024-09-10 19:45:46 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    // Display the lyrics for Unit Test 1
+    Console.WriteLine("Oh, well imagine");
+    Console.WriteLine("As i'm pacing the pews in a church corridor");
+    Console.WriteLine("And i can't help but to hear");
+    Console.WriteLine("No, i can't help but to hear an exchanging of words...");
+    Console.WriteLine();
+
+    // Display the lyrics for Unit Test 2
+    Console.WriteLine("Oh oh oh oh oh");
+    Console.WriteLine("oh oh oh oh");
+    Console.WriteLine("oh oh oh");
+    Console.WriteLine("Caught in a bad romance...");
+    Console.WriteLine("");
+
+    // Display the lyrics for Unit Test 3
+    Console.WriteLine("Coming out of my cage");
+    Console.WriteLine("And i've been doing just fine");
+    Console.WriteLine("Gotta gotta be dowm");
+    Console.WriteLine("Because i want it all");
+    Console.WriteLine("");
+
+    // Display the lyrics for Unit Test 4
+    Console.WriteLine("Now this is a story all about how");
+    Console.WriteLine("My life got flipped- turned upside down");
+    Console.WriteLine("And i'd like to take a minute");
+    Console.WriteLine("Just sit right there");
+    Console.WriteLine("I'll tell you how i became the prince of Bel-Air");
+  }
+}
